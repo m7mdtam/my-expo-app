3c0df76d7b7611a77e5a4b260f30f39a64e50a3a
m7mdtam <mohammedtamimi72@gmail.com>
2025-08-05 10:05:40 +0300

    exampleof dynmaic routing.

--- app/index.tsx
+++ app/index.tsx
@@ -8,7 +8,10 @@ export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
       <Text className="text-5xl text-primary font-bold">Hello</Text>
+     
       <Link href='/onboarding'>Onboarding</Link>
+      <Link href='/movie/avangers'>Avanger Movie</Link>
+
     </View>
   );
 }
